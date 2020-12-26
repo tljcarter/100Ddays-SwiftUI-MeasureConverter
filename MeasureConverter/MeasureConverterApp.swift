d3bf29b34442ea2ccf2a92c0c49532e311bdd447
Tom L carter <tom@tljcarterhome.com>
2020-12-26 15:06:10 -0600

    Initial Commit

---
+++ MeasureConverter/MeasureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureConverterApp.swift
+//  MeasureConverter
+//
+//  Created by Tom Carter on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct MeasureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
